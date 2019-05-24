e1213a1b49f7801f72e2dcde5017517edbf178ad
Karl Maier <karl.maier@link2brain.at>
2019-05-24 09:43:39 +0100

    very simple example to demonstrate split and merge

--- main.py
+++ main.py
@@ -1,61 +1,99 @@
 import skmultiflow as sk
-from timeseries import Timeseries, generate_multi_timeseries, get_timeseries_next_value, SinGenerator, CosGenerator
+from timeseries import Timeseries, SinGenerator, CosGenerator
 from tree import Node_of_tree
 from anytree.search import findall
 from anytree import RenderTree
 import numpy as np
 import math
 
+def print_tree(root_node, obs_number):
+    print("tree at observation #{}".format(i))
+    for pre, fill, node in RenderTree(root_node):
+        print("%s%s %s %s" % ( \
+            pre, \
+            node.name, \
+            node.statistics.dist_dict_coef.get('d1_val'), \
+            node.list_timeseries_names() if node.active_cluster else " [NOT ACTIVE]" ))
+
 print("#################")
 print("##### INIT. #####")
 print("#################")
 
-### generate some timeseries
-### every timeseries with an even id has a sine generator function
-### every timeseries with an odd id has a cosine generator function
-n_ts = 41
-np.random.seed(seed=42)
-series = generate_multi_timeseries(n_ts=n_ts)
+# generate some timeseries
+# S0-S1 with sine waves
+# S2-S7 with cosine waves
+n_ts = 8
+np.random.seed(seed=45)
+series = {}
+series['S0'] = Timeseries('S0', SinGenerator())
+series['S1'] = Timeseries('S1', SinGenerator())
+series['S2'] = Timeseries('S2', CosGenerator())
+series['S3'] = Timeseries('S3', CosGenerator())
+series['S4'] = Timeseries('S4', CosGenerator())
+series['S5'] = Timeseries('S5', CosGenerator())
+series['S6'] = Timeseries('S6', CosGenerator())
+series['S7'] = Timeseries('S7', CosGenerator())
 
-### Initialize root node of the tree (the initial cluster) and set timeseries to initial cluster
+# Initialize root node of the tree (the initial cluster) and set timeseries to initial cluster
 root_node = Node_of_tree('root_node')
 root_node.set_cluster_timeseries(series)
 
-### initial run: let the tree grow to cluster the timeseries
-### for each active cluster - get next value for each series inside the cluster and calculate and update statistics
+# initial run: let the tree grow to cluster the timeseries
+# for each active cluster - get next value for each series inside the cluster and calculate and update statistics
 
-for i in range(200):
+for i in range(1000):
     for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
         active_cluster.update_statistics()
         if active_cluster.test_split() or active_cluster.test_aggregate():
-            print("tree at observation #{}".format(i))
-            for pre, fill, node in RenderTree(root_node):
-                print("%s%s %s %s" % ( \
-                    pre, \
-                    node.name, \
-                    node.statistics.dist_dict_coef.get('d1_val'), \
-                    node.list_timeseries_names() if node.active_cluster else " [NOT ACTIVE]" ))
+            print_tree(root_node, i)
+
+print('#################')
+print_tree(root_node, i)
 
 print("#################")
-print("##### DRIFT #####")
+print("##### DRIFT 1 ###")
 print("#################")
 
-### simulate concept drift: in the lower half of the timeseries,
-### those with a cosine generator have a sine generator now as well
-for key, value in series.items():
-    if value.id < n_ts/2 and value.id%2 == 1:
-        print("{}: sine generator".format(value.name))
-        value.generator = SinGenerator()
+# We now change the type of S6 and S7 completely: they are now sine waves
+# we also introduce a little dissimilarity in the cluster S2-S5
 
-for i in range(200):
+
+series['S2'].generator = CosGenerator(start=0.1)
+series['S3'].generator = CosGenerator(start=0.2)
+series['S4'].generator = CosGenerator(start=0.3)
+series['S5'].generator = CosGenerator(start=0.4)
+
+series['S6'].generator = SinGenerator(start=0.1)
+series['S7'].generator = SinGenerator(start=0.4)
+
+for i in range(1000, 2000):
+    for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
+        active_cluster.update_statistics()
+        if active_cluster.test_split() or active_cluster.test_aggregate():
+            print_tree(root_node, i)
+
+print('#################')
+print_tree(root_node, i)
+
+print("#################")
+print("##### DRIFT 2 ###")
+print("#################")
+
+# We now change S6 and S7 back to sine waves
+# and add some more dissimilarity in the cluster S2-S5
+
+series['S4'].generator = SinGenerator(start=0.1)
+series['S5'].generator = SinGenerator(start=0.4)
+
+series['S6'].generator = SinGenerator(start=0.2)
+series['S7'].generator = SinGenerator(start=0.3)
+
+for i in range(2000,4000):
     #print("# " + str(i))
     for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
         active_cluster.update_statistics()
         if active_cluster.test_split() or active_cluster.test_aggregate():
-            print("tree at observation #{}".format(i))
-            for pre, fill, node in RenderTree(root_node):
-                print("%s%s %s %s" % ( \
-                    pre, \
-                    node.name, \
-                    node.statistics.dist_dict_coef.get('d1_val'), \
-                    node.list_timeseries_names() if node.active_cluster else " [NOT ACTIVE]" ))
+            print_tree(root_node, i)
+
+print('#################')
+print_tree(root_node, i)
--- timeseries.py
+++ timeseries.py
@@ -4,29 +4,31 @@ import numpy as np
 class SinGenerator:
     """Generator for a sine wave, with 10% random noise"""
 
-    def __init__(self, start = 0, inc = 0.1):
+    def __init__(self, start = 0, inc = 0.1, noise = 0.02):
         self.state = start
         self.inc = inc
+        self.noise = noise
 
     def next_val(self):
         self.state = self.state + self.inc
-        return np.sin(self.state) + np.random.randn()/10
+        return np.sin(self.state) + np.random.randn()*self.noise
 
 class CosGenerator:
     """Generator for a cosine wave, with 10% random noise"""
 
-    def __init__(self, start = 0, inc = 0.1):
+    def __init__(self, start = 0, inc = 0.1, noise = 0.02):
         self.state = start
         self.inc = inc
+        self.noise = noise
 
     def next_val(self):
         self.state = self.state + self.inc
-        return np.cos(self.state) + np.random.randn()/10
+        return np.cos(self.state) + np.random.randn()*self.noise
 
 class Timeseries:
     """A single timeseries, with an associated id, name and generator"""
 
-    def __init__(self, id, name, generator):
+    def __init__(self, name, generator):
         self.id = id
         self.name = name
         self.current_value = None
@@ -36,24 +38,3 @@ class Timeseries:
     def next_val(self):
         self.current_value = self.generator.next_val()
         return self.current_value
-
-####################################
-
-def generate_multi_timeseries(n_ts):
-    """Generates the specified number of timeseries. All timeseries with an odd id
-       are based on a sine wave and all timeseries with an even id on a cosine wave"""
-    dic_of_timeseries = {}
-    for i in range(0,n_ts):
-        name = "S"+str(i)
-        print("{}: {} generator".format(name, "sine" if i%2==0 else "cosine"))
-        dic_of_timeseries[name] = Timeseries( \
-            id=i, \
-            name=name, \
-            generator = SinGenerator() if i%2==0 else CosGenerator())
-    return dic_of_timeseries
-
-def get_timeseries_next_value(dic_of_timeseries):
-    next_values = []
-    for key, value in dic_of_timeseries.items():
-        next_values.append((key,value.next_val()))
-    return next_values
