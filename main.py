26b3137f8a56898269382f5ae6cfa39ca4c7797d
Karl Maier <karl.maier@link2brain.at>
2019-06-01 12:37:08 +0100

    further cleanup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Demonstration for clustering time series via ODAC algorithm"""
 import skmultiflow as sk
 from timeseries import Timeseries, SinGenerator
 from tree import Node_of_tree
@@ -24,15 +25,17 @@ series['S5'] = Timeseries('S5', SinGenerator(start=math.pi))
 series['S6'] = Timeseries('S6', SinGenerator(start=math.pi))
 series['S7'] = Timeseries('S7', SinGenerator(start=math.pi))
 
-# Initialize root node of the tree (the initial cluster) and set timeseries to initial cluster
+# Initialize root node of the tree (the initial cluster) and set
+# timeseries to initial cluster
 root_node = Node_of_tree('root_node')
 root_node.set_cluster_timeseries(series)
 
 # initial run: let the tree grow to cluster the timeseries
-# for each active cluster - get next value for each series inside the cluster and calculate and update statistics
-
+# for each active cluster - get next value for each series
+# inside the cluster and calculate and update statistics
 for i in range(1000):
-    for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
+    for active_cluster in \
+    findall(root_node,filter_=lambda node: node.active_cluster is True):
         active_cluster.update_statistics()
         if active_cluster.test_split() or active_cluster.test_aggregate():
             print("tree at observation #{}".format(i))
@@ -41,23 +44,23 @@ for i in range(1000):
 # we can observe, that the algorithm correctly clusters the time series
 # one cluster with S0-S1
 # one cluster with S2-S7
-
 print('#################')
 root_node.print()
 
 # Now, let's introduce a concept drift, that the algorithm has to adjust
-
 print("#################")
 print("##### DRIFT 1 ###")
 print("#################")
 
 # We now change the type of S6-S7 to be like S0-S1:
-
-series['S6'].generator = SinGenerator(start=series['S6'].generator.state + math.pi)
-series['S7'].generator = SinGenerator(start=series['S7'].generator.state + math.pi)
+series['S6'].generator = \
+    SinGenerator(start=series['S6'].generator.state + math.pi)
+series['S7'].generator = \
+    SinGenerator(start=series['S7'].generator.state + math.pi)
 
 for i in range(1000, 2000):
-    for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
+    for active_cluster in \
+    findall(root_node,filter_=lambda node: node.active_cluster is True):
         active_cluster.update_statistics()
         if active_cluster.test_split() or active_cluster.test_aggregate():
             print("tree at observation #{}".format(i))
@@ -68,32 +71,34 @@ for i in range(1000, 2000):
 # one with S6-S7
 # We know, that S6-S7 are now of the same generating structure as S0-S1
 # still, we are missing one criteria for aggregation:
-# the cluster size of the child cluster has to grow bigger than the parent cluster
-
+# the cluster size of the child cluster has to grow bigger than the
+# parent cluster
 print('#################')
 root_node.print()
 
 # Let's now change S6-S7 back to it's original generating function
 # that S2-S7 would be of the same shape
-# but in order for the aggregation function to apply, we need to increase the cluster sizes
+# but in order for the aggregation function to apply, we need to
+# increase the cluster sizes
 # that it is bigger than their parent clusters size + hoeffding bound
-
 print("#################")
 print("##### DRIFT 2 ###")
 print("#################")
 
 # We do this by adding some dissimilarity within the clusters
-
 series['S2'].generator = SinGenerator(start=series['S2'].generator.state - 0.4)
 series['S3'].generator = SinGenerator(start=series['S3'].generator.state - 0.2)
 series['S4'].generator = SinGenerator(start=series['S4'].generator.state + 0)
 series['S5'].generator = SinGenerator(start=series['S5'].generator.state + 0.2)
 
-series['S6'].generator = SinGenerator(start=series['S6'].generator.state - math.pi + 0.2)
-series['S7'].generator = SinGenerator(start=series['S7'].generator.state - math.pi - 0.3)
+series['S6'].generator = SinGenerator(start=series['S6'].generator.state \
+    - math.pi + 0.2)
+series['S7'].generator = SinGenerator(start=series['S7'].generator.state \
+    - math.pi - 0.3)
 
 for i in range(2000,3000):
-    for active_cluster in findall(root_node,filter_=lambda node: node.active_cluster is True):
+    for active_cluster in \
+    findall(root_node,filter_=lambda node: node.active_cluster is True):
         active_cluster.update_statistics()
         if active_cluster.test_split() or active_cluster.test_aggregate():
             print("tree at observation #{}".format(i))
@@ -102,6 +107,5 @@ for i in range(2000,3000):
 # We can see, that the cluster readjusts to model the right structure
 # one cluster with S0-S1
 # one cluster with S2-S7
-
 print('#################')
 root_node.print()
